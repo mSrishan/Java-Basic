2374a2d1d5c37110c64db8f841dd66bd033fdd0d
smkankanamge <119028384+mSrishan@users.noreply.github.com>
2024-08-03 00:12:02 +0530

    user input file update.

--- userInputIf.java
+++ userInputIf.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
+
 class userInputIf {
-	public static void main (String[] args){
+	public static void main(String[] args) {
 		Scanner age = new Scanner(System.in);
-		
+
 		System.out.print("Enter your age : ");
 		int userAge = age.nextInt();
-		
-		if(userAge >= 18) {
-			System.out.println("You can access");
+
+		if (userAge >= 18) {
+			System.out.println("You can access to our system.");
 		} else {
-			System.out.println("You can't access");
+			System.out.println("You can't access to our system.");
 		}
 	}
 }
