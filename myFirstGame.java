bb055f334632f9e2a14e6abef8a5aa9afecb4df8
smkankanamge <119028384+mSrishan@users.noreply.github.com>
2024-08-07 23:15:22 +0530

    myFirstGame.

---
+++ myFirstGame.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class myFirstGame {
+    public static void main(String[] args) {
+        Scanner uInput = new Scanner(System.in);
+        Random sNumber = new Random();
+
+        for (int i = 1; i < 30; i++) {
+            System.out.println("Player no : " + i);
+            for (int j = 0; j < 5; j++) {
+                System.out.print("Enter a number between 1 and 10 : ");
+                int uNumber = uInput.nextInt();
+                int rNumber = sNumber.nextInt(10);
+                if (uNumber == rNumber) {
+                    System.out.println("You have won the game");
+                    break;
+                } else {
+                    System.out.println("You have lost the game");
+                    System.out.println("The winning number is : " + rNumber);
+                }
+            }
+        }
+
+    }
+
+}
