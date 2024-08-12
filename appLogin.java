b8d07ec6b519f26e0c5952827d2236c6b7e9cdbd
smkankanamge <119028384+mSrishan@users.noreply.github.com>
2024-08-12 09:21:16 +0530

    Complete the appLogin.

--- appLogin.java
+++ appLogin.java
@@ -8,13 +8,19 @@ public class appLogin {
         Scanner input = new Scanner(System.in);
 
         for (int i = 0; i < 5; i++) {
+
             System.out.print("Enter the Email : ");
             String uEmail = input.next();
             System.out.print("Enter the password : ");
             String uPassword = input.next();
-            if (uEmail == dbEmail && uPassword == dbPassword) {
+            if (dbEmail.equals(uEmail) && dbPassword.equals(uPassword)) {
                 System.out.println("Welcome...");
                 break;
+            } else {
+                System.out.println("Try again...");
+            }
+            if (i == 4) {
+                System.out.println("Your account is locked.");
             }
         }
 
