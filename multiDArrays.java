46b4172f731c206500f0a17b7f3cacf81fe6daa0
smkankanamge <119028384+mSrishan@users.noreply.github.com>
2024-08-16 00:28:34 +0530

    multiDArrays.

---
+++ multiDArrays.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class multiDArrays {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[][] capacity = new int[225][3];
+
+        for (int i = 0; i < 225; i++) {
+            System.out.println("Minister number " + (i + 1) + " : ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print("Enter vehicle number " + (j + 1) + " : ");
+                capacity[i][j] = scan.nextInt();
+            }
+
+        }
+    }
+}
