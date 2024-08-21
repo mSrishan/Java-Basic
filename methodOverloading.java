d392f0b5a601976a87ae53e2999228aeac94a003
smkankanamge <119028384+mSrishan@users.noreply.github.com>
2024-08-21 23:32:38 +0530

    update

--- methodOverloading.java
+++ methodOverloading.java
@@ -1,16 +1,19 @@
 public class methodOverloading {
+
     public static void main(String[] args) {
+        System.out.println("Method Overloading......");
 
+        Employee emp1 = new Employee(23, "Srishan");
+        System.out.println(emp1.name);
     }
+}
 
-    public static void getSalary(int x) {
-    }
+class Employee {
+    int age;
+    String name;
 
-    public static void getSalary(int x, int y) {
-        
-    }
-
-    public static void getSalary(double x) {
-
+    public Employee(int x, String y) {
+        age = x;
+        name = y;
     }
 }
