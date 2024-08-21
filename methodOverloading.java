2317ed24dd5f5409d44bd5b995768672d4f53105
smkankanamge <119028384+mSrishan@users.noreply.github.com>
2024-08-21 23:09:48 +0530

    method overloading.

---
+++ methodOverloading.java
@@ -0,0 +1,16 @@
+public class methodOverloading {
+    public static void main(String[] args) {
+
+    }
+
+    public static void getSalary(int x) {
+    }
+
+    public static void getSalary(int x, int y) {
+        
+    }
+
+    public static void getSalary(double x) {
+
+    }
+}
